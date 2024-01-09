db8246f6172c69e045fe5acfbe2b32d9699d0470
Alex Jones <alexjonesdev@gmail.com>
2024-01-08 19:39:09 -0500

    Created empty project and files.

---
+++ src/constants.rs
@@ -0,0 +1,5 @@
+use bevy::prelude::*;
+
+pub const PADDLE_SIZE: Vec3 = Vec3::new(4.0, 1.0, 0.0);
+pub const BLOCK_SIZE: Vec2 = Vec2::new(2., 1.);
+pub const BALL_SIZE: f32 = 1.;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// Uses and includes
+use bevy::prelude::*;
+
+// Local modules
+mod constants;
+mod components;
+
+/// Main function
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run()
+}
+
+/// Spawn players and bases
+fn setup() {
+    // TODO
 }
